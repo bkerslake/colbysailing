37b8fb132ba9fa65695e35254716f564d25b8b78
bryn kerslake <brynkerslake@gmail.com>
2025-01-23 16:36:32 -0500

    first

---
+++ js/fetchscript.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://colby.dserec.com/api/clubsports/online/club_member/index/club/46?draw=1&columns%5B0%5D%5Bdata%5D=&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=false&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=&columns%5B1%5D%5Bname%5D=full_name&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=&columns%5B2%5D%5Bname%5D=year_in_school&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=&columns%5B3%5D%5Bname%5D=officer_position&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=2&order%5B0%5D%5Bdir%5D=desc&start=0&length=25&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1737667669112')
+      .then(response => response.json())
+      .then(data => {
+        const roster = document.getElementById('team-roster');
+        data.data.forEach(member => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+            <td><img src="images/default.jpg" alt="${member.first_name} ${member.last_name}" class="headshot"></td>
+            <td>${member.year_in_school}</td>
+            <td>${member.first_name} ${member.last_name}</td>
+            <td>Unknown</td>
+            <td>${member.officer_position}</td>
+          `;
+          roster.appendChild(row);
+        });
+      })
+      .catch(error => console.error('Error fetching team data:', error));
+  });
